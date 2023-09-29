7b74fa8715e02e2cf0740c70da0f48afa4b847e0
Santiago Suarez <santisuarezpungitore@hotmail.com>
2023-09-29 14:16:24 -0300

    Add compilation, simple run and gitignore

---
+++ compile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# exit in error
+set -e
+
+
+echo "Compiling libraries"
+echo
+
+libs=$(find ./src/ -regex '.*\.c$')
+for lib in "${libs[@]}"
+do
+	filename=$(basename ${lib} | sed -E 's/^(.*)\.c$/\1/')
+	echo 'Compiling file:' ${filename}.c
+	gcc -c ${lib} -o obj/${filename}.o
+done
+
+echo "Compiling main code"
+rm -f obj/main
+gcc $(find obj/ -type f) main.c -o obj/main
+
+
